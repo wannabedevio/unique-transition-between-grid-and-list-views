955c53dacf3b43fcc91ebe5f561003533540e2e9
MirzaHodzic <mirzahodzic88@gmail.com>
2023-06-18 18:34:02 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# a-unique-transition-between-grid-and-list-views
+# A Unique Transition Between Grid and List Views
+
 A tutorial on how to create a unique transition between grid and list views with CSS, Javascript, Masonry, and Greensock.
+
+![A Unique Transition Between Grid and List Views](https://raw.githubusercontent.com/wannabedevio/demo-images/main/unveiling-the-enchanting-view-mode-symphony-new.png)
+
+[Article on WANNABEDEV](https://wannabedev.io/tutorials/unveiling-the-enchanting-view-mode-symphony/)
+
+[Demo](https://wannabedev.io/_posts/demo/unveiling-the-enchanting-view-mode-symphony)
+
+## Credits
+- The inspiration for effects and Layout comes from [kreatıva](https://dribbble.com/shots/3777026-Spindle-Data-Sources-Animation)
+- [GSAP](https://greensock.com) by Greensock
+- [imagesLoaded](https://imagesloaded.desandro.com/) by David DeSandro
+- Font [Source Sans Pro](https://fonts.google.com/specimen/Source+Sans+Pro) by Google Fonts
+- Simple Loader by [loading.io](https://loading.io/css/)
+- [Color Palette](https://coolors.co/) by coolors.co
+- Prev and Next Icons made by [Freepik](https://www.freepik.com/) from [Flaticon](https://www.flaticon.com/) are licensed by [CC 3.0](http://creativecommons.org/licenses/by/3.0/)
+
+## License
+This resource can be used freely if integrated or build upon in personal or commercial projects such as websites, web apps and web templates intended for sale. It is not allowed to take the resource "as-is" and sell it, redistribute, re-publish it, or sell "pluginized" versions of it. Free plugins built using this resource should have a visible mention and link to the original work. Always consider the licenses of all included libraries, scripts and images used.
+
+## Misc
+
+Follow WANNABEDEV: [GitHub](https://github.com/wannabedevio), [Twitter](https://twitter.com/wannabedev_io), [Instagram](https://www.instagram.com/wannabedev.io/), [TikTok](https://www.tiktok.com/@wannabedev.io), 
+
+[© WANNABEDEV 2023](https://wannabedev.io)
---
+++ assets/js/demo.js
@@ -0,0 +1,156 @@
+/**
+ * demo.js
+ *
+ * Licensed under the MIT license.
+ * https://opensource.org/license/mit/
+ * 
+ * Copyright 2023, WANNABEDEV
+ * https://wannabedev.io
+ */
+
+ const handleViewModeSwitch = () => {
+  // Selectors
+  const gridSelector = '.grid';
+  const masonryItemSelector = '.masonry__item';
+  const loaderSelector = '.loader';
+  const dateSelector = '.date';
+  const tagsSelector = '.tags';
+  const listLayoutSelector = '.list-layout';
+  const gridLayoutSelector = '.grid-layout';
+
+  // Masonry initialization
+  new Masonry(gridSelector, {
+    itemSelector: masonryItemSelector
+  });
+
+  // Hide loader
+  setTimeout(() => {
+    document.querySelector(loaderSelector).classList.add('is-loaded');
+  }, 750);
+
+  // Assign special classes to first, second, and third items in the grid
+  const classes = ['top', 'middle', 'bottom'];
+  document.querySelectorAll(masonryItemSelector).forEach((element, index) => {
+    element.classList.add(classes[index % 3]);
+  });
+
+  // List layout function
+  const listLayout = (element) => {
+    // Initialize variables
+    const masterTimeline = gsap.timeline({ paused: true });
+    const topTimeline = gsap.timeline();
+    const middleTimeline = gsap.timeline();
+    const bottomTimeline = gsap.timeline();
+    const topPosition = parseInt(element.offsetTop);
+    const topPositionSecond = topPosition + 80;
+    const topPositionThird = topPosition + 160;
+
+    // Update position: top for each item in grid
+    if (element.classList.contains('top')) {
+      topTimeline.to(element, 1.05, { top: topPosition, ease: Expo.easeInOut });
+    }
+    if (element.classList.contains('middle')) {
+      middleTimeline.to(element, 1.05, { top: topPositionSecond, ease: Expo.easeInOut });
+    }
+    if (element.classList.contains('bottom')) {
+      bottomTimeline.to(element, 1.05, { top: topPositionThird, ease: Expo.easeInOut });
+    }
+
+    // Populate master timeline
+    masterTimeline
+      .to(element.querySelector(dateSelector), 0.35, { autoAlpha: 0, ease: Expo.easeInOut })
+      .to(element.querySelector(tagsSelector), 0.35, { autoAlpha: 0, ease: Expo.easeInOut }, '-=0.35')
+      .to(element, 0.35, { height: 80, ease: Expo.easeInOut })
+      .add(topTimeline, -0.35)
+      .add(middleTimeline, -0.35)
+      .add(bottomTimeline, -0.35)
+      .to(element, 0.35, { width: '100%', left: 0, ease: Expo.easeInOut })
+      .to(element.querySelector(dateSelector), 0.35, { autoAlpha: 1, ease: Expo.easeInOut })
+      .to(element.querySelector(tagsSelector), 0.35, { y: -24, autoAlpha: 1, ease: Expo.easeInOut }, '-=0.35');
+
+    // Play the animation
+    masterTimeline.play();
+  };
+
+  // Grid layout function
+  const gridLayout = (element) => {
+    // Initialize variables
+    const masterTimeline = gsap.timeline({ paused: true });
+    const topTimeline = gsap.timeline();
+    const middleTimeline = gsap.timeline();
+    const bottomTimeline = gsap.timeline();
+    const topPosition = parseInt(element.offsetTop);
+    const topPositionSecond = topPosition - 80;
+    const topPositionThird = topPosition - 160;
+
+    // Update position: left
+    if (element.classList.contains('top')) {
+      topTimeline.to(element, 1.05, { left: 0, ease: Expo.easeInOut });
+    }
+    if (element.classList.contains('middle')) {
+      middleTimeline.to(element, 1.05, { left: '33.3333%', ease: Expo.easeInOut });
+    }
+    if (element.classList.contains('bottom')) {
+      bottomTimeline.to(element, 1.05, { left: '66.6666%', ease: Expo.easeInOut });
+    }
+    // Update position: top (to prevent overlapping)
+    if (element.classList.contains('top')) {
+      topTimeline.to(element, 0.7, { top: topPosition, ease: Expo.easeInOut });
+    }
+    if (element.classList.contains('middle')) {
+      middleTimeline.to(element, 0.7, { top: topPositionSecond, ease: Expo.easeInOut });
+    }
+    if (element.classList.contains('bottom')) {
+      bottomTimeline.to(element, 0.7, { top: topPositionThird, ease: Expo.easeInOut });
+    }
+
+    // Populate master timeline
+    masterTimeline
+      .to(element.querySelector(dateSelector), 0.35, { autoAlpha: 0, ease: Expo.easeInOut })
+      .to(element.querySelector(tagsSelector), 0.35, { autoAlpha: 0, ease: Expo.easeInOut }, '-=0.35')
+      .to(element, 0.7, { width: '33.3333%', ease: Expo.easeInOut })
+      .add(topTimeline, -0.35)
+      .add(middleTimeline, -0.35)
+      .add(bottomTimeline, -0.35)
+      .to(element, 0.35, { height: 240, ease: Expo.easeInOut }, '-=0.5')
+      .to(element.querySelector(dateSelector), 0.35, { autoAlpha: 1, ease: Expo.easeInOut })
+      .to(element.querySelector(tagsSelector), 0.35, { y: 0, autoAlpha: 1, ease: Expo.easeInOut }, '-=0.35');
+
+    // Play the animation
+    masterTimeline.play();
+  };
+
+  // Grid to list trigger
+  document.querySelector(listLayoutSelector).addEventListener('click', () => {
+    // Add/remove active class
+    document.querySelector(gridLayoutSelector).classList.remove('active');
+    document.querySelector(listLayoutSelector).classList.add('active');
+    // Call list function
+    const elements = document.querySelectorAll(masonryItemSelector);
+    elements.forEach((element) => {
+      listLayout(element);
+    });
+  });
+
+  // List to grid trigger
+  document.querySelector(gridLayoutSelector).addEventListener('click', () => {
+    // Add/remove active class
+    document.querySelector(listLayoutSelector).classList.remove('active');
+    document.querySelector(gridLayoutSelector).classList.add('active');
+    // Call grid function
+    const elements = document.querySelectorAll(masonryItemSelector);
+    elements.forEach((element) => {
+      gridLayout(element);
+    });
+  });
+
+  // Extras - Click on item to show/hide tags
+  const tags = document.querySelectorAll(tagsSelector);
+  tags.forEach((tag) => {
+    tag.querySelector('li:nth-child(1) a').classList.add('active');
+    tag.querySelector('li:nth-child(2) a').classList.add('active');
+  });
+};
+
+// Call the function
+handleViewModeSwitch();
